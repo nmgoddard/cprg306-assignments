552b231854e111d874a4ced98941609acd9fa6e3
nmgoddard <149209419+nmgoddard@users.noreply.github.com>
2024-09-13 10:09:23 -0600

    Fixes
    - Corrected link to connect to whole repository
    - Renamed website title

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,8 +4,8 @@ export default function StudentInfo(){
     return(
         <div className = "flex flex-col justify-center items-center p-3">
             <p>Hello! My name is Natalia.</p>
-            <Link href="https://github.com/nmgoddard/cprg306-assignments" 
-            className="text-cyan-500 underline hover:text-cyan-200 hover:no-underline">This is the link to the repository for this project.</Link>
+            <Link href="https://github.com/nmgoddard" 
+            className="text-cyan-500 underline hover:text-cyan-200 hover:no-underline">This is the link to my Github repository.</Link>
         </div>
     );
 }
